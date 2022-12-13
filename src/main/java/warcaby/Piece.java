efc1c136738ca76400c77f858fcf6ca58f5038b7
Agata Groszek <agata@groszek.mobi>
2022-12-13 22:57:56 +0100

    Modyfikacja klas Piece, Square

--- src/main/java/warcaby/Piece.java
+++ src/main/java/warcaby/Piece.java
@@ -1,4 +1,27 @@
 package warcaby;
 
 public abstract class Piece {
+    private boolean killed = false;
+    private boolean white = false;
+
+    public Piece(boolean white) {
+        this.white = white;
+    }
+
+    public boolean isWhite() {
+        return white;
+    }
+
+    public void setWhite(boolean white) {
+        this.white = white;
+    }
+
+    public boolean isKilled() {
+        return killed;
+    }
+
+    public void setKilled(boolean killed) {
+        this.killed = killed;
+    }
+
 }
