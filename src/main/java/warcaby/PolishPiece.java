27115056b6b25d53feeedeb7b05c8626fb4ab343
patrycjaaann <115889312+patrycjaaann@users.noreply.github.com>
2023-01-08 17:58:01 +0100

    Update PolishPiece.java

--- src/main/java/warcaby/PolishPiece.java
+++ src/main/java/warcaby/PolishPiece.java
@@ -1,14 +1,8 @@
 package warcaby;
 
-//import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 
-//import java.util.List;
-
 public class PolishPiece extends Piece{
-    //private int oldX, oldY;
-    //private int moveDirection;
-    //private State state;
     
     PolishPiece(int x, int y, int r, Color color, State state) {
         super(x,y,r);
@@ -23,30 +17,6 @@ public class PolishPiece extends Piece{
         this.state = state;
     }
 
-    /*public int getOldX() {
-        return oldX;
-    }
-
-    public int getOldY() {
-        return oldY;
-    }
-
-    public int getMoveDirection() {
-        return moveDirection;
-    }
-
-    public Color getColor() {
-        return (Color) getFill();
-    }
-
-    public State getState() {
-        return this.state;
-    }
-
-    public void setState(State newState) {
-        this.state = newState;
-    }*/
-
     @Override
     public void move(int newX, int newY) {
         oldX = newX*70;
@@ -59,22 +29,4 @@ public class PolishPiece extends Piece{
             setStrokeWidth(5);
         }
     }
-
-    /*public void moveKing(int newX, int newY) {
-        oldX = newX*70;
-        oldY = newY*70;
-        setCenterX(oldX+35);
-        setCenterY(oldY+35);
-    }
-
-    public void notMove() {
-        setCenterX(oldX+35);
-        setCenterY(oldY+35);
-    }
-
-    public List<Square> getAvailableMoves(Square[][] board) {
-        return state.availableMoves(this, board);
-    }*/
 }
-
-// w pom było maven 1.7, a nie 1.8
