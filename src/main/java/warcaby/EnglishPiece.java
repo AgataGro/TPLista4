aae672a8caaf097a70d1065fb238b261aa2cdab2
patrycjaaann <115889312+patrycjaaann@users.noreply.github.com>
2023-01-13 22:29:38 +0100

    Update EnglishPiece.java

--- src/main/java/warcaby/EnglishPiece.java
+++ src/main/java/warcaby/EnglishPiece.java
@@ -4,6 +4,13 @@ import javafx.scene.paint.Color;
 
 public class EnglishPiece extends Piece {
 
+    /**
+     * @param x one of the coordinates of the middle of the circle
+     * @param y one of the coordinates of the middle of the circle
+     * @param r the value of circle radius
+     * @param color color of a piece - can be black or white
+     * @param state state of a piece - piece can become a king
+     */
     EnglishPiece(int x, int y, int r, Color color, State state) {
         super(x,y,r);
         setFill(color);
