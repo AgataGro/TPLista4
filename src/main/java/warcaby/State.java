7aeedb4993b51a537e0b994fda22e88319104fa3
patrycjaaann <115889312+patrycjaaann@users.noreply.github.com>
2023-01-16 01:55:18 +0100

    Update State.java

--- src/main/java/warcaby/State.java
+++ src/main/java/warcaby/State.java
@@ -17,7 +17,25 @@ public interface State {
      * @return lista pól, na które można przesunąć pionek
      */
     public List<SingleMove> availibleMoves(Piece piece, Square[][] board);
+    
+    /**
+     * @param piece piece which sequence of available moves is returned
+     * @param board array of aquares
+     * @param steps list of move sequences
+     * @return list of lists of move sequences
+     */
     public List<List<SingleMove>> moveSequence(Piece piece, Square[][] board, List<SingleMove> steps);
+    
+    /**
+     * @return state after change
+     */
     public State changeState();
+    
+    /**
+     * @param piece piece to move
+     * @param tiles array of squares
+     * @param direction direction in which the piece wants to move
+     * @return list of available move sequences
+     */
     public List<SingleMove> move(Piece piece, Square[][] tiles, Direction direction);
 }
